4d6a43332d524eeca02a4c6c8e230d322678987a
Y7gn <malekkhodary@hotmail.com>
2023-07-02 04:10:00 +0300

    first push

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TableAddCourse from "../components/TableAddCourse";
+
+const HomePage = () => {
+  return (
+    <>
+      <TableAddCourse />
+    </>
+  );
+};
+
+export default HomePage;
